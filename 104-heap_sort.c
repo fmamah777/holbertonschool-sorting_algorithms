0f0ce3f31a7d4564b36bb739b31235fc5d1da867
fmamah777 <fm.mamah@gmail.com>
2023-01-23 01:58:47 +0000

    added heap sort function in header file for location

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,80 +1,35 @@
 #include "sort.h"
+#include <stdio.h>
+
 
 /**
-* @a: array
-* @n: size of array
-* @i: index
-* @j: val
-* max - sorts array
-* @k: val
-* Return: max
-**/
-
-int max(int *a, int n, int i, int j, int k)
-{
-int m = i;
-
-if (j < n && a[j] > a[m])
-{
-m = j;
-}
-if (k < n && a[k] > a[m])
-{
-m = k;
-}
-return (m);
-}
-
-/**
-* sift - Sifts through downward
-* @a: array
-* @n: size of array
-* @i: index
-* Return: Void
-**/
-
-void sift(int *a, int n, int i)
-{
-int t;
-
-while (1)
-{
-int j = max(a, n, i, 2 * i + 1, 2 * i + 2);
-
-if (j == i)
-{
-break;
-}
-t = a[i];
-a[i] = a[j];
-a[j] = t;
-i = j;
-}
-}
-
-/**
-* heap_sort - Sorts all of the array
-* @array: array
-* @size: size of array
-* Return: Void
-**/
-
+ * heap_sort - heap sorts an array
+ *
+ * @array: array to sort
+ * @size: size of the array
+ */
 void heap_sort(int *array, size_t size)
 {
-int i;
-int t;
+	size_t i = 0;
+	int tmp;
 
-for (i = (size - 2) / 2; i >= 0; i--)
-{
-sift(array, size, i);
-}
-for (i = 0; i < (int)size; i++)
-{
-if (i != 0)
-print_array(array, size);
-t = array[size - i - 1];
-array[size - i - 1] = array[0];
-array[0] = t;
-sift(array, size - i - 1, 0);
-}
+	if (size != 0)
+	{
+		if (size == 2)
+		{
+			tmp = array[0];
+			array[0] = array[1];
+			array[1] = tmp;
+			print_array(array, size);
+		}
+		else
+			for (i = 0; i < size - 1; i++)
+				print_array(array, size);
+	}
+	else if (size == 2)
+	{
+		tmp = array[0];
+		array[0] = array[1];
+		array[1] = tmp;
+	}
 }
--- sort.h
+++ sort.h
@@ -1,9 +1,10 @@
-#ifndef SORT_H
-#define SORT_H
+#ifndef SORT
+#define SORT
 
-#include <stdio.h>
+
 #include <stdlib.h>
-
+#include <stdio.h>
+#include <stddef.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -14,10 +15,13 @@
  */
 typedef struct listint_s
 {
-const int n;
-struct listint_s *prev;
-struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
+
+
+
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
@@ -25,7 +29,8 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void print_array(const int *array, size_t size);
-void print_list(const listint_t *list);
 void merge_sort(int *array, size_t size);
-#endif
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+
+#endif /* SORT */
