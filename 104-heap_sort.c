82776892d929497e62074e9e5d51e164f6490b13
fmamah777 <fm.mamah@gmail.com>
2023-01-23 02:14:25 +0000

    syntax corrections

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,35 +1,80 @@
 #include "sort.h"
-#include <stdio.h>
-
 
 /**
- * heap_sort - heap sorts an array
- *
- * @array: array to sort
- * @size: size of the array
- */
+* max - Sorts array
+* @a: array
+* @n: size of array
+* @i: index
+* @j: val
+* @k: val
+* Return: max
+**/
+
+int max(int *a, int n, int i, int j, int k)
+{
+int m = i;
+
+if (j < n && a[j] > a[m])
+{
+m = j;
+}
+if (k < n && a[k] > a[m])
+{
+m = k;
+}
+return (m);
+}
+
+/**
+* sift - Sifts down
+* @a: array
+* @n: size of array
+* @i: index
+* Return: Void
+**/
+
+void sift(int *a, int n, int i)
+{
+int t;
+
+while (1)
+{
+int j = max(a, n, i, 2 * i + 1, 2 * i + 2);
+
+if (j == i)
+{
+break;
+}
+t = a[i];
+a[i] = a[j];
+a[j] = t;
+i = j;
+}
+}
+
+/**
+* heap_sort - Sorts array
+* @array: array
+* @size: size of array
+* Return: Void
+**/
+
 void heap_sort(int *array, size_t size)
 {
-	size_t i = 0;
-	int tmp;
+int i;
+int t;
 
-	if (size != 0)
-	{
-		if (size == 2)
-		{
-			tmp = array[0];
-			array[0] = array[1];
-			array[1] = tmp;
-			print_array(array, size);
-		}
-		else
-			for (i = 0; i < size - 1; i++)
-				print_array(array, size);
-	}
-	else if (size == 2)
-	{
-		tmp = array[0];
-		array[0] = array[1];
-		array[1] = tmp;
-	}
+for (i = (size - 2) / 2; i >= 0; i--)
+{
+sift(array, size, i);
+}
+for (i = 0; i < (int)size; i++)
+{
+if (i != 0)
+print_array(array, size);
+t = array[size - i - 1];
+array[size - i - 1] = array[0];
+array[0] = t;
+sift(array, size - i - 1, 0);
+}
 }
