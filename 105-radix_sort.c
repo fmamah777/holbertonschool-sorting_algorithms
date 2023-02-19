221d51c54928307dbf896848115c913ad36c3925
fmamah777 <fm.mamah@gmail.com>
2023-02-19 21:23:46 +0000

    compile corrections

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -1,70 +1,84 @@
 #include "sort.h"
+
 /**
- * get_max - get max element
- * @a: array
- * @n: size
- * Return: int
+ * count_sort - Uses count sort to sort an array by LSD
+ *
+ * @array: Array to sort, contains only numbers >= 0
+ * @size: Number of items in array
+ * @sig_dig: significant digit to sort by
  */
-
-int get_max(int *a, size_t n)
+void count_sort(int *array, size_t size, size_t sig_dig)
 {
-size_t i;
-int max = a[0];
+	int key_arr[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+	int index_arr[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+	int *buffer = malloc(sizeof(*array) * size);
+	size_t i, key;
 
-for (i = 1; i < n; i++)
-{
-if (a[i] > max)
-{
-max = a[i];
-}
+	for (i = 0; i < size; i++)
+	{
+		key = array[i] % (sig_dig * 10) / sig_dig;
+		key_arr[key]++;
+	}
 
-}
-return (max);
+	for (i = 0; i < 9; i++)
+	{
+		index_arr[i + 1] = index_arr[i] + key_arr[i];
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		key = array[i] % (sig_dig * 10) / sig_dig;
+		buffer[index_arr[key]] = array[i];
+		index_arr[key]++;
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		array[i] = buffer[i];
+	}
+	free(buffer);
 }
 
 /**
- * radix_sort - sorts an array of integers in ascending order
- * @array: array to sort
- * @size: size of array
- * Return: void
+ * get_max - gets max value in array
+ *
+ * @array: array of ints
+ * @size: length of array
+ * Return: max int
+ */
+size_t get_max(int *array, size_t size)
+{
+	size_t i;
+	int max = array[0];
+
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] > max)
+		{
+			max = array[i];
+		}
+	}
+	return (max);
+}
+
+/**
+ * radix_sort - Uses LSD Radix to sort an array of integers in ascending order
+ *
+ * @array: Array to sort, contains only numbers >= 0
+ * @size: Number of items in array
  */
 void radix_sort(int *array, size_t size)
 {
-size_t bucket[10][10], bucket_cnt[10];
-size_t i, j, k, r, NOP = 0, divisor = 1, lar, pass;
+	size_t sig_dig;
+	int max;
 
-if (array == NULL || size < 2)
-return;
-
-lar = get_max(array, size);
-
-while (lar > 0)
-{
-NOP++;
-lar /= 10;
-}
-for (pass = 0; pass < NOP; pass++)
-{
-for (i = 0; i < 10; i++)
-{
-bucket_cnt[i] = 0;
-}
-for (i = 0; i < size; i++)
-{
-r = (array[i] / divisor) % 10;
-bucket[r][bucket_cnt[r]] = array[i];
-bucket_cnt[r] += 1;
-}
-i = 0;
-for (k = 0; k < 10; k++)
-{
-for (j = 0; j < bucket_cnt[k]; j++)
-{
-array[i] = bucket[k][j];
-i++;
-}
-}
-divisor *= 10;
-print_array(array, size);
-}
+	if (array && size > 1)
+	{
+		max = get_max(array, size);
+		for (sig_dig = 1; max / sig_dig > 0; sig_dig *= 10)
+		{
+			count_sort(array, size, sig_dig);
+			print_array(array, size);
+		}
+	}
 }
--- sort.h
+++ sort.h
@@ -1,10 +1,7 @@
 #ifndef SORT_H
 #define SORT_H
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdlib.h>
-#include <stddef.h>
 
+#include <stdlib.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -20,41 +17,18 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-
-void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
-
+void print_list(const listint_t *list);
 
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+int *quick_sort_recursive(int *array, int low, int high, int size);
 void quick_sort(int *array, size_t size);
-void sort_helper(int *array, int *temp, size_t size);
-
-
-void cocktail_sort_list(listint_t **list);
-void counting_sort(int *array, size_t size);
-
-
-
-void swap(int *a, int *b);
-void quick_sort_rec(int *array, int start, int end);
-void swap_list(const listint_t *a, const listint_t *b);
-void quick_sort_rec(int *array, int start, int end);
+void swap(listint_t **head, listint_t **node1, listint_t **node2);
 
 void merge_sort(int *array, size_t size);
-void merge_sort_helper(int *array, int *temp, size_t size);
-void merger(int *l, size_t size_l, int *r, size_t size_r, int *tmp);
-
 void heap_sort(int *array, size_t size);
-void heapify(int *array, size_t size, size_t i);
-void heap_sort_rec(int *array, size_t size);
-
-int maxIntInArray(int arr[], int n);
-int init_arr_len(int *arr_len);
-void radix_sort(int arr[], int n);
-
-
-int linear_search(int *array, size_t size, int value);
+void radix_sort(int *array, size_t size);
 
 #endif
